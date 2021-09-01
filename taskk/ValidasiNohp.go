fb05d44a29747eceafdd4a4d2c8579d356e156c1
Mirfann13 <45013528+Mirfann13@users.noreply.github.com>
2021-09-02 01:09:35 +0700

    revisi jadi menggunakan regex
    revisi jadi menggunakan regex

--- taskk/ValidasiNohp.go
+++ taskk/ValidasiNohp.go
@@ -1,10 +1,17 @@
 package main7
 
 import (
-	"github.com/dongri/phonenumber"
+	"regexp"
 )
 
 func ValidasiNohp(nohp string) string {
-	number := phonenumber.Parse(nohp, "id")
-	return number
+	number := regexp.MustCompile(`^(\+62|62)?[\s-]?0?8[1-9]{1}\d{1}[\s-]?\d{4}[\s-]?\d{2,5}$`)
+	m := regexp.MustCompile("^(.*?)0(.*)$")
+	str := "${1}62$2"
+	res := m.ReplaceAllString(nohp, str)
+
+	if number.MatchString(nohp) {
+		return res
+	}
+	return "ERROR"
 }
